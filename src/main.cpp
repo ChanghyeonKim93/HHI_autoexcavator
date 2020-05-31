41f26e0345cb227a350f1487f409477102aeac19
ChanghyeonKim93 <hyun91015@gmail.com>
2020-05-31 21:14:27 +0900

    keyboard input okay.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <termios.h> // UNIX LINUX.
 #include <ros/ros.h>
+
+#include "keyinput.h"
 
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
@@ -7,6 +10,7 @@
 #include <image_transport/image_transport.h>
 
 // custom msgs
+// ref: https://steemit.com/kr-dev/@jacobyu/1303-ros-custom-message-generation
 #include "hhi_autoexcavator/hhi_msgs.h" // dedicated msgs for HHI project.
 
 #include <Eigen/Dense>
@@ -28,10 +32,32 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "hhi_gcs");
     ros::NodeHandle nh("~");
     
+    // publish message topic to control all module nodes.
     ros::Publisher pub_hhi_msgs = 
         nh.advertise<hhi_autoexcavator::hhi_msgs>("/hhi/msg",1);
     
-    while(ros::ok()){
+    // 
+    string common_phrase =
+    "\n==================\n Press a key...\n   i: query one scene.\n   q: cease the program \n Select an input: \n";
+    cout << common_phrase;
+    while(ros::ok())
+    {
+        int c = getch(); // call my own non-blocking input function
+        if(c == 'i'){
+            cout << "\n\n snapshot the current scene.\n";
+            // Do something here!
+
+            cout << common_phrase;
+        }
+        else if(c == 'q'){
+            cout <<"\n\n quit program\n";
+            return -1;
+        }
+        else if((c != 0))
+        {
+            cout << ": unknown command...\n";
+            cout << common_phrase;
+        }
 
         ros::spinOnce();
     }
