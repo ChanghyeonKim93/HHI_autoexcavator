e18e1925f3cb2db8ac693381b942503a43ef7ce4
ChanghyeonKim93 <hyun91015@gmail.com>
2020-05-31 19:25:45 +0900

    add main.cpp and the basis of the program is generated.

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ros/ros.h>
+
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+
+#include <Eigen/Dense>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <thread>
+#include <mutex>
+
+int main(int argc, char **argv) {
+    std::cout << "hhi_gcs_node is running..." << std::endl;
+    ros::init(argc, argv, "hhi_gcs_node");
+    ros::NodeHandle nh("~");
+
+
+
+
+
+    while(ros::ok()){
+
+        ros::spinOnce();
+    }
+
+ROS_INFO_STREAM("GCS off\n");
+return -1;
+}
